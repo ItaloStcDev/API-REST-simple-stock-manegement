679f176214ddd8055b111c48b4a3b99b59ede006
ItaloStcDev <italostcdev@gmail.com>
2026-07-16 14:25:36 -0300

    Atualizar o db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,10 +1,10 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'stockv2_db',
-    password: 'admin',
+    user: 'example',
+    host: 'example',
+    database: 'example',
+    password: 'example',
     port: 5432
 })
 
