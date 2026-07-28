70c11463c51a04e906c4042a6241a88fef3bb739
ItaloStcDev <italostcdev@gmail.com>
2026-07-28 17:38:10 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const env = process.env;
 const port = env.APP_PORT;
 
 const corsOptions = {
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    origin: env.FRONT_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
